bfa5dd5907f7daa6ed216be851866cfd824a377c
Rahul Reddy <rahulreddyp77@gmail.com>
2019-02-11 23:26:02 +0530

    Disburse models added

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from firstapp.models import Signup, Customer, Business, B_Docs, C_Docs,StatusCustomer, StatusBusiness
+from firstapp.models import Signup, Customer, Business, B_Docs, C_Docs, StatusCustomer, StatusBusiness, Loan,\
+    Investor, Accepted_Customers, Repayment, Delinquency,\
+    Accepted_Business, B_Verification, Business_Invoice_Details, C_Verification, UT_Money, VirtualPayment
 
 # Register your models here.
 admin.site.register(Signup)
 admin.site.register(Business)
 admin.site.register(Customer)
+admin.site.register(Business_Invoice_Details)
 admin.site.register(B_Docs)
 admin.site.register(C_Docs)
 admin.site.register(StatusCustomer)
 admin.site.register(StatusBusiness)
+admin.site.register(B_Verification)
+admin.site.register(C_Verification)
+admin.site.register(Loan)
+admin.site.register(Investor)
+admin.site.register(Accepted_Business)
+admin.site.register(Accepted_Customers)
+admin.site.register(VirtualPayment)
+admin.site.register(Repayment)
+admin.site.register(Delinquency)
+admin.site.register(UT_Money)
--- firstapp/models.py
+++ firstapp/models.py
@@ -96,3 +96,98 @@ class StatusBusiness(models.Model):
     b_id = models.ForeignKey(Business, on_delete=True, blank=True)
     ap_id = models.ForeignKey(Signup, on_delete=True, blank=True)
     status = models.CharField(default='False', max_length=20)
+
+# Verification Table
+class B_Verification(models.Model):
+    ap_id = models.ForeignKey(Signup, on_delete=models.CASCADE, blank=True)
+    b_id = models.ForeignKey(Business, on_delete=models.CASCADE, blank=True)
+    ml_accuracy = models.FloatField(blank=True)
+    ml_status = models.CharField(default='F', max_length=1)
+    ml_remarks = models.TextField(default='verified')
+    mv_accuracy = models.CharField(default='F', max_length=1)
+    mv_status = models.CharField(default='F', max_length=1)
+    mv_remarks = models.TextField(default='verified')
+    final_status = models.CharField(default='F', max_length=1)
+
+class C_Verification(models.Model):
+    ap_id = models.ForeignKey(Signup, on_delete=models.CASCADE, blank=True)
+    b_id = models.ForeignKey(Business, on_delete=models.CASCADE, blank=True)
+    c_id = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=True)
+    ml_accuracy = models.FloatField(blank=True)
+    ml_status = models.CharField(default='F', max_length=1)
+    mv_accuracy = models.FloatField(blank=True)
+    mv_status = models.CharField(default='F', max_length=1)
+    final_status = models.CharField(default='F', max_length=1)
+
+class VirtualPayment(models.Model):
+    vpa = models.BigIntegerField(unique=True)
+    b_id = models.ForeignKey(Business, on_delete=models.CASCADE, blank=True)
+    c_id = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=True)
+    created_date = models.DateTimeField(default=datetime.now(), blank=True)
+    expiry_date = models.DateTimeField(blank=True)
+    amount = models.FloatField(blank=True)
+
+class Loan(models.Model):
+    ap_id = models.ForeignKey(Signup, on_delete=models.CASCADE, blank=True)
+    b_id = models.ForeignKey(Business, on_delete=models.CASCADE, blank=True)
+    loan_id = models.AutoField(primary_key=True)
+    approved_date = models.DateTimeField(default=datetime.now())
+    total_amount = models.FloatField(blank=True)
+    one_disburse_amount = models.FloatField(blank=True)
+    one_disburse_date = models.DateTimeField(blank=True)
+    two_disburse_amount = models.FloatField(blank=True)
+    two_disburse_date = models.DateTimeField(blank=True)
+    investor_id = models.BigIntegerField(blank=True)
+    status = models.CharField(max_length=10, blank=True)
+
+class Accepted_Customers(models.Model):
+    ap_id = models.ForeignKey(Signup, on_delete=models.CASCADE, blank=True)
+    c_id = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=True)
+    b_id = models.ForeignKey(Business, on_delete=models.CASCADE, blank=True)
+    invoice_amount_paid = models.FloatField(blank=True)
+    loan_id = models.ForeignKey(Loan, on_delete=models.CASCADE, blank=True)
+    amount_due_date = models.DateField(blank=True)
+    amount_paid_date = models.DateTimeField(blank=True)
+    vpa = models.BigIntegerField(blank=True)
+
+class Accepted_Business(models.Model):
+    ap_id = models.ForeignKey(Signup, on_delete=models.CASCADE, blank=True)
+    b_id = models.ForeignKey(Business, on_delete=models.CASCADE, blank=True)
+    invoice_amount = models.FloatField(blank=True)
+    sanctioned_amount = models.FloatField(blank=True)
+    loan_id = models.ForeignKey(Loan, on_delete=models.CASCADE, blank=True)
+    paid_date = models.DateTimeField(blank=True)
+    vpa = models.BigIntegerField(blank=True)
+
+class Repayment(models.Model):
+    loan_id = models.ForeignKey(Loan, on_delete=models.CASCADE, blank=True)
+    c_id = models.ForeignKey(Accepted_Customers, on_delete=models.CASCADE, blank=True)
+    req_amount = models.FloatField(blank=True)
+    paid_amount = models.FloatField(blank=True)
+    repay_date = models.DateTimeField(default=datetime.now(), blank=True)
+    rp_status = models.CharField(default='F', max_length=1)
+    rp_remarks = models.TextField()
+
+class Investor(models.Model):
+    investor_id = models.AutoField(primary_key=True)
+    investor_amount = models.CharField(max_length=30, blank=True)
+    amount_invested = models.FloatField(blank=True)
+    amount_earned = models.FloatField(blank=True)
+    loan_id = models.ForeignKey(Loan, on_delete=models.CASCADE, blank=True)
+
+class UT_Money(models.Model):
+    investor_id = models.ForeignKey(Investor, on_delete=models.CASCADE, blank=True)
+    loan_id = models.ForeignKey(Loan, on_delete=models.CASCADE, blank=True)
+    amount_debited = models.FloatField(blank=True)
+    intrst_amount = models.FloatField(blank=True)
+    amount_credited = models.FloatField(blank=True)
+    total_amount = models.FloatField(blank=True)
+
+class Delinquency(models.Model):
+    loan_id = models.ForeignKey(Loan, on_delete=models.CASCADE, blank=True)
+    b_id = models.ForeignKey(Business, on_delete=models.CASCADE, blank=True)
+    c_id = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=True)
+    c_invoice_amount = models.FloatField(blank=True)
+    c_invoice_due_date = models.DateField(blank=True)
+    c_invoice_paid_date = models.DateTimeField(blank=True)
+    delinquency_status = models.CharField(default='False', max_length=10)
