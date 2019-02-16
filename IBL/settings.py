aaf9d0e7be6d4130749acf18ff4b0ffe347878e7
Rahul Reddy <rahulreddyp77@gmail.com>
2019-02-16 22:10:50 +0530

    Virtual payment and Validations

--- IBL/settings.py
+++ IBL/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import pytz
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'formtools',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
